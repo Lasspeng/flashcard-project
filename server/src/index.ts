3cdba998b8f76997bf7b76400c64e5f141ebb1b4
Gavin Liburd <GavinLiburd901@gmail.com>
2022-11-16 19:03:32 -0500

    Create endpoint for get request

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,15 +3,23 @@ import express, { Request, Response } from 'express';
 import mongoose, { Mongoose } from 'mongoose';
 
 import Deck from './models/Deck';
+import cors from 'cors';
 
 // Hide credentials for Mongo database access
 import { config } from 'dotenv';
 config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT: number = 4000;
+
+app.get('/decks', async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  console.log(decks);
+  res.json(decks);
+});
 
 app.post('/decks', async (req: Request, res: Response) => {
   const newDeck = new Deck({ title: req.body.title });
